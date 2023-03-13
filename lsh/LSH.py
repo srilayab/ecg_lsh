3c64aa80957a2f5f9fec1dfd1c74061b735607c1
Srilaya Bhavaraju <srilaya.bhavaraju08@gmail.com>
2023-03-12 22:08:42 -0400

    File clean up - minus large data file

--- lsh/LSH.py
+++ lsh/LSH.py
@@ -1,3 +1,4 @@
+import math
 class LSH:
 
     '''
@@ -5,30 +6,42 @@ class LSH:
     k: number of neighbors
     L: number of hash_functions
     '''
-    def __init__(self, data, hash_family, k, L, distance_metric, thresholds=None):
-        self.hash_family = hash_family(data, L, )
+    def __init__(self, data, hash_family, k):
+        self.data = data
+        self.hash_family = hash_family(data)
         self.k = k
-        self.L = L
-        self.distance_metric = distance_metric
-        self.hash_tables = self.hash_family.create_hash_tables(data, thresholds)
+        self.L = 5
+        self.hash_tables = self.hash_family.get_hash_tables()
+
 
     def query(self, q):
         q_hash = self.hash_family.hash(q)
         candidate_set = set()
         for l in range(self.L):
             q_feature = q_hash[l]
+            if q_feature == math.inf:
+                continue
             hash_table = self.hash_tables[l]
+            if q_feature not in hash_table:
+                continue
             matching_candidates = hash_table[q_feature]
-            candidate_set.add(matching_candidates)
+            candidate_set.update(candidate[0] for candidate in matching_candidates)
         k_nearest = self.closest_neighbors(q, candidate_set)
+        return k_nearest
 
     def closest_neighbors(self, q, candidate_set):
+        q = q[0].tolist()
         distances = []
-        for candidate in candidate_set:
-            distance = self.distance_metric(q, candidate)
-            distances.append((distance, candidate))
+        for i in candidate_set:
+            candidate = self.data[i][0].tolist()
+            distance = self.euclidean(q, candidate)
+            distances.append((distance, i))
         distances.sort()
         return [distances[i][1] for i in range(self.k)]
+
+    def euclidean(self, list1, list2):
+        inside_squares = [(a-b) ** 2 for a, b in zip(list1, list2)]
+        return sum(inside_squares) ** .5
 
 
 
--- lsh/LSH_families.py
+++ lsh/LSH_families.py
@@ -1,4 +1,65 @@
 import random
+import sklearn
+from sklearn.datasets import make_blobs
+import numpy as np
+class E2LSHFamily:
+    def __init__(self, data, chosen_feature=0 , num_hash_functions=5):
+        self.data = data
+        self.w = num_hash_functions
+        self.chosen_feature = chosen_feature
+        self.dim_per_feature = len(data[0][chosen_feature])
+        self.hash_tables, self.r, self.b = self.create_hash_tables()
+
+    def get_hash_tables(self):
+        return self.hash_tables
+
+    def get_r(self):
+        X,y = make_blobs(n_samples=self.dim_per_feature, cluster_std=1, random_state=1)
+        r = []
+        for d in range(self.dim_per_feature):
+            feat = X[d]
+            r.append(d)
+        return r
+
+    def get_b(self, w):
+        return random.randint(0, w)
+
+    def create_hash_tables(self):
+        hash_tables = []
+        r_vectors = []
+        b_vectors = []
+        for i in range(1, self.w+1):
+            hash_function = {}
+            r = self.get_r()
+            b = self.get_b(self.w)
+            r_vectors.append(r)
+            b_vectors.append(b)
+            for (j, data_point) in enumerate(self.data):
+                feat_1 = data_point[self.chosen_feature]
+                hash_value = round((np.dot(feat_1, r) + b)/i, -3)
+                if hash_value in hash_function:
+                    hash_function[hash_value].append((j, data_point))
+                else:
+                    hash_function[hash_value] = [(j, data_point)]
+            hash_tables.append(hash_function)
+        return hash_tables, r_vectors, b_vectors
+
+    def hash(self, vector):
+        feat = vector[self.chosen_feature]
+        hash_keys = []
+        for i in range(1, len(self.hash_tables)+1):
+            r = self.r[i-1]
+            b = self.b[i-1]
+            w = i
+            hash_key = (np.dot(feat, r) + b)/w
+            hash_keys.append(round(hash_key, -3))
+        return hash_keys
+
+
+
+
+
+
 class L1HashFamily:
     def __init__(self, data, num_hash_functions, thresholds):
         self.num_hash_functions = num_hash_functions
